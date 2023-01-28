f4ca07f938cd6d626bd81f97d51373aaa8cde48b
Sardor <sardorsher1337@gmail.com>
2023-01-29 00:57:44 +0500

    Added Entites

---
+++ NajotEdu/NajotEdu.Domain/Entities/Group.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NajotEdu.Domain.Entities
+{
+    public class Group
+    {
+        public Group()
+        {
+            Lessons = new HashSet<Lesson>();
+            GroupStudents = new HashSet<StudentGroup>();
+        }
+
+        public int Id { get; set; }
+        public int TeacherId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public User Teacher { get; set; }
+        public ICollection<Lesson> Lessons { get; set; }
+        public ICollection<StudentGroup> GroupStudents { get; set; }
+        
+    }
+}
---
+++ NajotEdu/NajotEdu.Domain/Entities/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NajotEdu.Domain.Entities
+{
+    public class Student
+    {
+        public Student()
+        {
+            StudentGroups = new HashSet<StudentGroup>();
+            Attendances = new HashSet<Attendance>();
+        }
+
+        public int Id { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public DateTime BirthDate { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+        public ICollection<StudentGroup> StudentGroups { get;set; }
+        public ICollection<Attendance> Attendances { get; set; }
+    }
+}
